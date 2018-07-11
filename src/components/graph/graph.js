34b3ea07680bed4cafed5265af4b0fe9f38e61b9
Ahmad <a7m4d.m.sh@gmail.com>
2018-07-11 18:44:26 +0300

    edit char

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -1,8 +1,7 @@
 import './style.css';
 import React from 'react';
-
+import { Bar } from 'react-chartjs-2';
 const Graph = (props) => {
-
   const data = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
@@ -25,14 +24,16 @@ const Graph = (props) => {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: [12, 15]
       }
     ]
   };
-
-
+let exercise = props.exercise;
+let result = exercise.weight / ( 1.0278 - 0.0278 * exercise.reps );
+data.datasets[0].data.push(result);
+console.log(data.datasets[0].data);
       return (
-        <Bar data={data} />
+      <Bar data={data} />
 )
 }
 
